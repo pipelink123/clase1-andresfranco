07dfd400ee013ea5fe0702d2ab9fdffeeb7b9b42
Andres_universe <info@eniun.coclear>
2022-07-05 12:28:36 -0500

    NavBar-Tienda-andresfranco

---
+++ src/components/Navbar.js
@@ -0,0 +1,53 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function NavBar() {
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            global.org
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Inicio
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Nosotros
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  conocenos
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  productos
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      
+    </>
+  );
+}
+export default NavBar;
